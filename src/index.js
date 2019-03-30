c88f9e53ea2a14bf38e14d59c9808d1a27143267
Marek Kras <marek4004@wp.pl>
2019-03-30 15:33:13 +0100

    początek zajęc

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {lat: null}
+    }
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+            err => console.log(err)
+        )
+        return<div>Latitude: {this.state.lat}</div>
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector("#root"))
